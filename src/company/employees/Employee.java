dd11db32fffb8ca0246b28c2060d864a11c85822
karabo <mawaiskara@gmail.com>
2025-03-16 22:16:54 +0200

    finished with the mandatory work

--- src/company/employees/Employee.java
+++ src/company/employees/Employee.java
@@ -2,5 +2,18 @@
 package company.employees;
 
 public class Employee {
+    String name;
+    int id;
+    double salary;
+    public Employee(String name, int id, double salary){
+        this.name = name;
+        this.id = id;
+        this.salary = salary;
+    }
 
+    public void displayInfo(){
+        System.out.println("name: " + name);
+        System.out.println("ID: " + id);
+        System.out.println("salary: " + salary);
+    }
 }
